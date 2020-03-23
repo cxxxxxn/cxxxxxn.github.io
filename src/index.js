8ad6a0d940b815271739df674370e2b830bb5447
Christy <>
2020-03-23 15:41:36 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Cxxxxxn from './Cxxxxxn';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Cxxxxxn />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
