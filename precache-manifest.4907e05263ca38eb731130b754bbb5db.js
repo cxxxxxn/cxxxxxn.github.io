ac9df51b9f3c617ac38c92bd9c954a9e2d37789a
Christy <>
2020-03-24 21:46:26 +0800

    clear

---
+++ precache-manifest.4907e05263ca38eb731130b754bbb5db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f07f659004fc10c42e606e11778775",
+    "url": "/index.html"
+  },
+  {
+    "revision": "109466a11c5f8b7b7931",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "716cdfa2d19e6eb5e984",
+    "url": "/static/css/main.b5fc711f.chunk.css"
+  },
+  {
+    "revision": "109466a11c5f8b7b7931",
+    "url": "/static/js/2.c4a8c701.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4a8c701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "716cdfa2d19e6eb5e984",
+    "url": "/static/js/main.c73c42c1.chunk.js"
+  },
+  {
+    "revision": "d81196cbd19ac1d20a2e",
+    "url": "/static/js/runtime-main.ef4c655a.js"
+  },
+  {
+    "revision": "52169d8d98fa2027607261043f51415c",
+    "url": "/static/media/avatar.52169d8d.jpg"
+  },
+  {
+    "revision": "e0f5a9e0aeffa1911a1cb17a87b13242",
+    "url": "/static/media/the_road_to_react.e0f5a9e0.md"
+  }
+]);
