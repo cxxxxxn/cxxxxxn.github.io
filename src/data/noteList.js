5699079091cebff4d5c772c7ae57e3ac98ad137a
Christy <>
2020-08-24 22:20:46 +0800

    note

--- src/data/noteList.js
+++ src/data/noteList.js
@@ -1,8 +1,32 @@
+import js from './js.md';
+import react_and_redux1 from './react_and_redux1.md';
+import react_and_redux2 from './react_and_redux2.md';
 import the_road_to_react1 from './the_road_to_react1.md';
 import the_road_to_react2 from './the_road_to_react2.md';
 import third_party_library_react from './third_party_library_react.md';
 
 const noteList = [{
+    'name':'react_and_redux2',
+    'title':'深入浅出React和Redux 笔记（2）',
+    'desc': '4-6节 （模块化React和Redux应用、React组件的性能优化、React高级组件）',
+    'time':'2020-4-14',
+    'type':'React',
+    'md':react_and_redux2
+},{
+    'name':'javascript',
+    'title':'JavaScript New To Me',
+    'desc': '关于js我还没完全掌握的知识。继续学习并巩固⛽️！',
+    'time':'2020-4-13',
+    'type':'JavaScript',
+    'md':js
+},{
+    'name':'react_and_redux1',
+    'title':'深入浅出React和Redux 笔记（1）',
+    'desc': '2-3节 （高质量的React组件、从Flux到React），初次接触react-redux。',
+    'time':'2020-4-9',
+    'type':'React',
+    'md':react_and_redux1
+},{
     'name':'third_party_library_react',
     'title':'好用的第三方库',
     'desc': '记录下那些我在项目中过的好用的第三方库，方便之后再想用的时候查找。',
